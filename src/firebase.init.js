069bbc6281eb99694fbf51284c896aa856ece757
frsiam <siam.frs@gmail.com>
2022-05-24 13:20:08 +0600

    initialize the client side

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBVDPAifj4sVd-k_s3hAvu8p2vDwnSWpp4",
+    authDomain: "crystal-computers-tech.firebaseapp.com",
+    projectId: "crystal-computers-tech",
+    storageBucket: "crystal-computers-tech.appspot.com",
+    messagingSenderId: "320443687788",
+    appId: "1:320443687788:web:82049be70acf9a40ba0949"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
