195cd96ef1097a7f1ea3d7c249133343d48b5bb1
frsiam <siam.frs@gmail.com>
2022-05-24 15:23:54 +0600

    .env.local file create  and firebase project key make  is secure

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,17 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBVDPAifj4sVd-k_s3hAvu8p2vDwnSWpp4",
-    authDomain: "crystal-computers-tech.firebaseapp.com",
-    projectId: "crystal-computers-tech",
-    storageBucket: "crystal-computers-tech.appspot.com",
-    messagingSenderId: "320443687788",
-    appId: "1:320443687788:web:82049be70acf9a40ba0949"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export default auth;
