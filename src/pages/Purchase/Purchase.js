357fce9dfe5fc44a10224edb75290a6edccbbfee
frsiam <siam.frs@gmail.com>
2022-05-26 01:04:03 +0600

    purchase page is added

--- src/pages/Purchase/Purchase.js
+++ src/pages/Purchase/Purchase.js
@@ -1,13 +1,78 @@
-import React from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useParams } from 'react-router-dom';
+import auth from '../../firebase.init';
+import Loading from '../Shared/Loading';
 
 const Purchase = () => {
     const { id } = useParams();
-    const navigate = useNavigate();
+    const [user] = useAuthState(auth)
+    const [isLoading, setIsLoading] = useState(true)
+
+    const [purchase, setPurchase] = useState(false);
+    const [orderQuantity, setOrderQuantity] = useState(0)
+
+    const [part, setPart] = useState({});
+    useEffect(() => {
+        fetch(`http://localhost:4000/purchase/${id}`)
+            .then(res => res.json())
+            .then(data => {
+                setPart(data)
+                setOrderQuantity(data.minOrderQuantity)
+                setIsLoading(false)
+            })
+    }, [])
+
+    if (isLoading) {
+        return <Loading />
+    }
+    const { minOrderQuantity, availableQuantity, img, price, description, name } = part;
+
+    const handleQunatity = event => {
+        const order = event.target.value;
+        const numOrder = parseFloat(order)
+        setOrderQuantity(numOrder)
+
+        if ((numOrder) < minOrderQuantity || (numOrder) > availableQuantity) {
+            console.log(numOrder)
+            return setPurchase(true)
+
+        }
+        console.log(orderQuantity)
+        setPurchase(false)
+        // console.log('out side if')
+    }
+
+    const handleOrder = (event) => {
+        event.preventDefault();
+
+    }
     return (
-        <div className='h-[calc(100vh-200px)]'>
-            <h1>Here is the purchase section: {id}</h1>
-        </div>
+        <>
+            <div class="hero min-h-screen bg-base-200">
+                <div class="hero-content flex-col lg:flex-row-reverse">
+                    <div class="text-center lg:text-left">
+                        <h1 class="text-5xl font-bold">{name}</h1>
+                        <p class="py-6">{description}</p>
+                    </div>
+                    <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <div class="card-body">
+                            <h1 className='text-3xl font-bold text-center'>Order Form</h1>
+                            <form onSubmit={handleOrder} className='grid grid-cols-1 gap-4 justify-items-center mt-5'>
+                                <input name='name' disabled type="text" placeholder="Your Name" className="input input-bordered w-full max-w-xs" value={user?.displayName || ''} />
+
+                                <input name='email' disabled value={user?.email || ''} type="email" placeholder="Your Email" className="input input-bordered w-full max-w-xs" />
+
+                                <input onChange={handleQunatity} value={orderQuantity} name='quantity' type="number" placeholder={part.minOrderQuantity} className="input input-bordered w-full max-w-xs" />
+
+                                <input name='phone' type="number" placeholder="Phone Number" className="input input-bordered w-full max-w-xs" />
+                                <button disabled={purchase} type='submit' className={`btn ${purchase ? 'btn-accent' : 'btn-secondary'} w-full max-w-xs`}>Order</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
--- src/pages/Shared/Header.js
+++ src/pages/Shared/Header.js
@@ -1,12 +1,16 @@
 import { signOut } from 'firebase/auth';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { Link, Navigate, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
+import Loading from './Loading';
 
 const Header = () => {
-    const [user] = useAuthState(auth);
+    const [user, loading] = useAuthState(auth);
     const navigate = useNavigate();
+    if (loading) {
+        return <Loading />
+    }
     const menuItems = <>
         <li><Link to='/home'>Home</Link></li>
         <li><Link to='/appointment'>Appointment</Link></li>
@@ -16,6 +20,7 @@ const Header = () => {
         {
             user && <li><Link to='/dashboard'>Dashboard</Link></li>
         }
+        <li>{user && <span className='text-lg font-bold text-slate-300'>{user.displayName.split(' ').reverse()[0]}</span>}</li>
         <li>{user ? <button onClick={() => {
             signOut(auth);
             navigate('/login')
@@ -25,7 +30,7 @@ const Header = () => {
     </>
     return (
         <div className="navbar bg-gradient-to-r from-purple-500 to-pink-500">
-            <div className="navbar-start">
+            <div className="navbar">
                 <div className="dropdown">
                     <label tabIndex="0" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
@@ -36,7 +41,7 @@ const Header = () => {
                 </div>
                 <Link to='' className="btn btn-ghost normal-case text-xl">Crystal Computers</Link>
             </div>
-            <div className="navbar-end hidden lg:flex">
+            <div className="navbar hidden lg:flex">
                 <ul className="menu menu-horizontal p-0">
                     {menuItems}
                 </ul>
